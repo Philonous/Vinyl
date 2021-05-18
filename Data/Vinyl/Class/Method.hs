7e761b94b848d3dc6e7287c345f840b32d6c37ac
Philipp Balzarek <p.balzarek@googlemail.com>
2021-05-18 19:46:37 +0200

    Fix mapFields

--- Data/Vinyl/Class/Method.hs
+++ Data/Vinyl/Class/Method.hs
@@ -28,7 +28,7 @@ module Data.Vinyl.Class.Method
   ( -- * Mapping methods over records
     RecMapMethod(..)
   , rmapMethodF
-  -- , mapFields
+  , mapFields
   , RecMapMethod1(..)
   , RecPointed(..)
   , rtraverseInMethod
@@ -142,7 +142,7 @@ data FieldTyper = FieldId | FieldSnd
 -- | The interpretation function of the 'FieldTyper' symbols.
 type family ApplyFieldTyper (f :: FieldTyper) (a :: k) :: * where
   ApplyFieldTyper 'FieldId a = a
-  ApplyFieldTyper 'FieldSnd '(s, b) = b
+  ApplyFieldTyper 'FieldSnd a = Snd a
 
 -- | A mapping of record contexts into the 'FieldTyper' function
 -- space. We explicitly match on 'ElField' to pick out the payload
@@ -212,14 +212,14 @@ rmapMethodF :: forall c f ts. (Functor f, FieldPayload f ~ 'FieldId, RecMapMetho
 rmapMethodF f = rmapMethod @c (fmap f)
 {-# INLINE rmapMethodF #-}
 
--- -- | Apply a typeclass method to each field of a 'FieldRec'. This is a
--- -- specialization of 'rmapMethod'.
--- mapFields :: forall c ts. RecMapMethod c ElField ts
---            => (forall a. c a => a -> a) -> FieldRec ts -> FieldRec ts
--- mapFields f = rmapMethod @c g
---   where g :: c (PayloadType ElField t) => ElField t -> ElField t
---         g (Field x) = Field (f x)
--- {-# INLINE mapFields #-}
+-- | Apply a typeclass method to each field of a 'FieldRec'. This is a
+-- specialization of 'rmapMethod'.
+mapFields :: forall c ts. RecMapMethod c ElField ts
+           => (forall a. c a => a -> a) -> FieldRec ts -> FieldRec ts
+mapFields f = rmapMethod @c g
+  where g :: c (PayloadType ElField t) => ElField t -> ElField t
+        g (Field x) = Field (f x)
+{-# INLINE mapFields #-}
 
 -- | Like 'rtraverseIn', but the function between functors may be
 -- constrained.
